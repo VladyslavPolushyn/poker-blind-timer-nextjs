1b4f48da19c4caa5173109a12170b7794afa8770
Vladyslav Polushyn <polexodus@gmail.com>
2021-11-29 17:13:36 +0200

    feat: mooved table data to redux store

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -2,40 +2,12 @@ import { FC, useState } from 'react';
 import { Table } from 'react-bootstrap';
 import TableItem from '../src/components/TableItem/TableItem';
 import styles from './../styles/SettingsPage.module.scss';
-
-interface TableItemTypes {
-  round: number;
-  sb: number;
-  bb: number;
-  ante: number;
-  roundTime: number;
-}
+import { TableData } from '../src/types';
+import { useSelector } from 'react-redux';
+import { RootState } from 'typesafe-actions';
 
 const SettingsPage: FC = () => {
-  const [tableData, setTableData] = useState([
-    { round: 1, sb: 5, bb: 10, ante: 0, roundTime: 12 },
-    { round: 2, sb: 10, bb: 20, ante: 0, roundTime: 12 },
-    { round: 3, sb: 20, bb: 40, ante: 0, roundTime: 12 },
-    { round: 4, sb: 25, bb: 50, ante: 0, roundTime: 12 },
-    { round: 5, sb: 50, bb: 100, ante: 0, roundTime: 12 },
-    { round: 6, sb: 75, bb: 150, ante: 0, roundTime: 12 },
-    { round: 7, sb: 100, bb: 200, ante: 0, roundTime: 12 },
-    { round: 8, sb: 150, bb: 300, ante: 0, roundTime: 12 },
-    { round: 9, sb: 200, bb: 400, ante: 0, roundTime: 12 },
-    { round: 10, sb: 300, bb: 600, ante: 0, roundTime: 12 },
-  ]);
-
-  const onInputChangeHandler = ({
-    round,
-    sb,
-    bb,
-    ante,
-    roundTime,
-  }: TableItemTypes) => {
-    const newTableData = [...tableData];
-    newTableData[round - 1] = { round, sb, bb, ante, roundTime };
-    setTableData(newTableData);
-  };
+  const { tableData } = useSelector((state: RootState) => state);
 
   return (
     <div className={styles.SettingsPage}>
@@ -52,7 +24,7 @@ const SettingsPage: FC = () => {
             </tr>
           </thead>
           <tbody className={styles.Table__body}>
-            {tableData.map((item) => (
+            {tableData.map((item: TableData) => (
               <TableItem
                 key={item.round}
                 round={item.round}
@@ -60,7 +32,6 @@ const SettingsPage: FC = () => {
                 bb={item.bb}
                 ante={item.ante}
                 roundTime={item.roundTime}
-                onInputChangeHandler={onInputChangeHandler}
               />
             ))}
           </tbody>
